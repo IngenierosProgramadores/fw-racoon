76211362848174e74377db850d60a1eecc87d768
Raul Belloso M <raul.belloso.m@gmail.com>
2022-06-28 18:42:38 -0500

    feat (front) create buttons section

--- js/navbar.js
+++ js/navbar.js
@@ -1,15 +1,16 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 const navbarItem = document.getElementsByClassName("navbar-item")[0];
-
+// Navbar responsive
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
 navbarItem.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
-
+// Efect on scroll navbar
 window.addEventListener("scroll", function () {
   var header = document.querySelector("nav");
   header.classList.toggle("sticky", window.scrollY > 0);
+  // header.classList.toggle("navbar-fixed", window.scrollY > 30);
 });
